c55841e0b184e944a8ced680b14b96797c0b6868
Rob Fox <rfox@mailfence.com>
2025-06-17 11:17:12 +0800

    bump version, update author

--- scryfall_cache/scryfall_cache.py
+++ scryfall_cache/scryfall_cache.py
@@ -21,9 +21,9 @@ log = logging.getLogger(__name__)
 
 
 # Module information
-__version__ = "0.2.2"
-__author__ = """Max Dymond"""
-__email__ = "cmeister2@gmail.com"
+__version__ = "0.2.3"
+__author__ = """Rob Fox"""
+__email__ = "fox.robert92@gmail.com"
 package = "scryfall_cache"
 
 
